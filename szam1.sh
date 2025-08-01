c4d8dad5064f40010e14ca3f11b5644b0580d25b
AFilkor <filkor.adam@gmail.com>
2025-08-01 16:30:35 +0200

    1

---
+++ szam1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#adatbevitel
+read -p 'elso szam érteke:' a
+echo A szam érteke $a
+read -p 'A masodik szam érteke:' b
+echo B szam érteke $b
+#egyenloeke
+if(( $a==$b))
+then
+echo egyenloek
+#kisebb,nagyobb
+elif(( $a>$b))
+then
+echo "$a" nagyobb
+else
+echo "$b" nagyobb
+fi
